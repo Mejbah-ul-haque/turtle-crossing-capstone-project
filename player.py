23ad2cdb2b4609651e0bfd42981adbbc7da94402
md-mejbaul-haque <mejba.s@gmail.com>
2025-07-09 00:17:15 +0600

    Turtle moves to up

--- player.py
+++ player.py
@@ -9,5 +9,10 @@ class Player(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
 
-
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
